a59bed3aa67f008b4ea3afdd32a7565c5f98e334
Andrew <andrey@ob-vious.com>
2020-01-22 23:11:09 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <FollowersBlock
+      :data="mockData"
+      @on-update-data="updateData"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from './components/HelloWorld.vue'
+import FollowersBlock from '@/components/FollowersBlock.vue'
+import { MockData, mockData } from '@/utils'
 
 @Component({
   components: {
+    FollowersBlock,
     HelloWorld,
-  },
+  }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  mockData: MockData = mockData
+
+  updateData () {
+
+  }
+}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+  width: fit-content
+  margin: 60px auto
 </style>
---
+++ src/components/FollowersBlock.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="followers-block">
+    <div class="title-block">
+      <div class="title-block__sub-type">{{ displaySettings.subType }}</div>
+      <div class="title-block__type">{{ displaySettings.type }}</div>
+    </div>
+
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import { MockData } from '@/utils'
+
+@Component({})
+export default class FollowersBlock extends Vue {
+  @Prop({ default: () => ({}) }) mockData!: MockData
+
+  get displaySettings () {
+    return this.mockData.displaySettings
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
